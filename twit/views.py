cf1227dedbd7f09a108e741c115eb402b69977d9
kwaw Annor <kwawannor@gmail.com>
2013-09-08 17:12:07 +0000

    mobile update manager modified

--- twit/views.py
+++ twit/views.py
@@ -7,14 +7,15 @@ from twit.analyser import TwitterAnalyser
 
 def get_tweets(request, access_key):
 	if access_key == 'w398734jhje32kncy36ghsv':
-		#twitter, data, users, texts  = TwitterAnalyser(), twitter.get_data(), twitter.get_users(), twitter.get_text()
-		#update_db(users, texts)
+		twitter=TwitterAnalyser()
+		data, users, texts  =  twitter.get_data(), twitter.get_users(), twitter.get_text()
+		update_db(users, texts)
 		return render('twit/base.html', dict())
 	return HttpResponseRedirect('/')
 
 def update_db(users, texts):
 	schools = Schools.objects.all()
-	tweets  = Tweets.objectsa.all()
+	tweets  = Tweets.objects.all()
 	schools_names = [school.name for school in schools]
 	all_users = [tweet.user for tweet in tweets]
 	for i in range(len(texts)):
@@ -23,17 +24,17 @@ def update_db(users, texts):
 
 		if school_name in schools_names:
 			school = schools.get(name = school_name)
-			school.rank += 1
+			school.rank +=1
 			school.save()
 
 		else:
-			school = Schools.objects.create(name = school_name, rank = 1)
+			school = Schools.objects.create(name=school_name, rank=1)
 
-		if user in all_users:
-			user = tweets.get(user = user)
-			user.no_tweets += 1
-			user.save()
-			user = Tweets.objects.create(text = texts[i].encode('utf-8'), user = user[i], no_tweets = 1)
+		# if user in all_users:
+		# 	user = tweets.get(user = user)
+		# 	user.no_tweets += 1
+		# 	user.save()
+		# 	user = Tweets.objects.create(text = texts[i].encode('utf-8'), user = user[i], no_tweets = 1)
 
 			
 
